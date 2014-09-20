4b7eb4c1eef508213ffdce11edbfa7a18108b15d
Recep ASLANTAS <m@recp.me>
2014-09-20 19:12:20 +0300

    style: re-indent spaces

--- include/sig.h
+++ include/sig.h
@@ -1,17 +1,17 @@
-/* Copyright (c) 2014, Recep ASLANTAS <info@recp.me>
-*
-* Permission to use, copy, modify, and/or distribute this software for any
-* purpose with or without fee is hereby granted, provided that the above
-* copyright notice and this permission notice appear in all copies.
-*
-* THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-* WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-* MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-* ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-* WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-* ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-*/
+/* Copyright (c) 2014, Recep Aslantas <info@recp.me>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
 
 #ifndef libsig_signal_h
 #define libsig_signal_h
@@ -21,9 +21,9 @@
 #endif
 
 #ifdef __cplusplus
-#define	 __SIG_C_DECL extern "C"
+#define __SIG_C_DECL extern "C"
 #else
-#define	__SIG_C_DECL
+#define __SIG_C_DECL
 #endif
 
 typedef const void * const sig_signal_id_t;
@@ -32,7 +32,7 @@ typedef struct sig_signal_s sig_signal_t;
 typedef struct sig_signal_context_s sig_signal_context_t;
 
 struct sig_signal_context_s {
-	int ctx_id;
+  int ctx_id;
 };
 
 /* Predefined signal contexts */
@@ -40,12 +40,12 @@ extern sig_signal_context_t sig_ctx_default;
 extern sig_signal_context_t sig_ctx_sys;
 
 struct sig_signal_s {
-	sig_signal_id_t signal_id;
-	sig_signal_object_t object;
-	sig_signal_context_t * context;
+  sig_signal_id_t signal_id;
+  sig_signal_object_t object;
+  sig_signal_context_t * context;
 #ifdef __cplusplus
   sig_signal_s(sig_signal_id_t _signal_id)
-	: signal_id(_signal_id)  { }
+    : signal_id(_signal_id)  { }
 #endif
 };
 
@@ -91,27 +91,27 @@ __SIG_C_DECL void sig_fire_s(const char * signal, void * object);
 
 #ifdef __cplusplus
 namespace sig {
-	struct attach_stream_base_t {
-		virtual attach_stream_base_t & operator << (sig_observer_cb_t cb) const;
-		virtual attach_stream_base_t & operator >> (sig_observer_cb_t cb) const;
-	};
-	
-	struct attach_stream_signal_base_t {
-		attach_stream_base_t operator [ ] (const char * signal) const;
-		attach_stream_base_t operator [ ] (int signal) const;
-	};
-	
-	struct fire_stream_base_t {
-		fire_stream_base_t & operator << (void * object) const;
-	};
-	
-	struct fire_stream_signal_base_t {
-		fire_stream_base_t  operator [ ] (const char * signal) const;
-		fire_stream_base_t  operator [ ] (int signal) const;
-	};
-	
-	extern attach_stream_signal_base_t attach;
-	extern fire_stream_base_t fire;
+  struct attach_stream_base_t {
+    virtual attach_stream_base_t & operator << (sig_observer_cb_t cb) const;
+    virtual attach_stream_base_t & operator >> (sig_observer_cb_t cb) const;
+  };
+  
+  struct attach_stream_signal_base_t {
+    attach_stream_base_t operator [] (const char * signal) const;
+    attach_stream_base_t operator [] (int signal) const;
+  };
+  
+  struct fire_stream_base_t {
+    fire_stream_base_t & operator << (void * object) const;
+  };
+  
+  struct fire_stream_signal_base_t {
+    fire_stream_base_t operator [] (const char * signal) const;
+    fire_stream_base_t operator [] (int signal) const;
+  };
+  
+  extern attach_stream_signal_base_t attach;
+  extern fire_stream_base_t fire;
 };
 #endif
 
