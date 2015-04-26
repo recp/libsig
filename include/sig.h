05f276275ca12cf150de9c3d7680523a110e92ae
Recep ASLANTAS <m@recp.me>
2015-04-26 14:12:35 +0300

    declare detach functions

--- include/sig.h
+++ include/sig.h
@@ -88,6 +88,33 @@ __SIG_C_DECL void sig_attachc_s(int signal,
 #endif
 
 /**
+ * Detach a non-function or member-function to event
+ */
+__SIG_C_DECL void sig_detach(int signal, sig_observer_cb_t cb);
+__SIG_C_DECL void sig_detachc(int signal,
+                              sig_observer_cb_t cb,
+                              const sig_context_t * ctx);
+
+#ifdef __cplusplus
+// Default context
+void sig_detach(int signal, sig_observer_cb2_t cb);
+void sig_detach(const char * signal, sig_observer_cb2_t cb);
+
+// Default by given context
+void sig_detach(int signal,
+                sig_observer_cb2_t cb,
+                const sig_context_t * ctx);
+void sig_detach(const char * signal,
+                sig_observer_cb2_t cb,
+                const sig_context_t * ctx);
+#else
+__SIG_C_DECL void sig_detach_s(const char * signal, sig_observer_cb_t cb);
+__SIG_C_DECL void sig_detachc_s(int signal,
+                                sig_observer_cb_t cb,
+                                const sig_context_t * ctx);
+#endif
+
+/**
  * Fire an event by given name and object
  */
 __SIG_C_DECL void sig_fire(int signal, void * object);
