b42644d8a3c658b3ba860f202ae4464795dd2f81
Recep ASLANTAS <m@recp.me>
2015-04-28 21:55:16 +0300

    style

--- src/sig.cc
+++ src/sig.cc
@@ -8,30 +8,37 @@
 #include "sig.h"
 #include "sig-ctx.h"
 
-__SIG_C_DECL void sig_attach(int signal, sig_observer_cb_t cb) {
+__SIG_C_DECL void
+sig_attach(int signal, sig_observer_cb_t cb) {
   sig::__sig_def_ctx_mngr.add(signal, cb);
 }
 
-void sig_attach(int signal, sig_observer_cb2_t cb) {
+void
+sig_attach(int signal, sig_observer_cb2_t cb) {
   sig::__sig_def_ctx_mngr.add(signal, cb);
 }
 
-void sig_attach(const char * signal, sig_observer_cb2_t cb) {
+void
+sig_attach(const char * signal, sig_observer_cb2_t cb) {
   sig::__sig_def_ctx_mngr_s.add(signal, cb);
 }
 
-__SIG_C_DECL void sig_attach_s(const char * signal, sig_observer_cb_t cb) {
+__SIG_C_DECL void
+sig_attach_s(const char * signal, sig_observer_cb_t cb) {
   sig::__sig_def_ctx_mngr_s.add(signal, cb);
 }
 
-__SIG_C_DECL void sig_fire(int signal, void * object) {
+__SIG_C_DECL void
+sig_fire(int signal, void * object) {
   sig::__sig_def_ctx_mngr.fire(signal, object);
 }
 
-void sig_fire(const char * signal, void * object) {
+void
+sig_fire(const char * signal, void * object) {
   sig::__sig_def_ctx_mngr_s.fire(signal, object);
 }
 
-__SIG_C_DECL void sig_fire_s(const char * signal, void * object) {
+__SIG_C_DECL void
+sig_fire_s(const char * signal, void * object) {
   sig::__sig_def_ctx_mngr_s.fire(signal, object);
 }
